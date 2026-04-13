d93874fc8f39c82b8fa328e7369fd2d459acd43c
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-04-13 14:55:24 +0530

    first commit

--- studyplanner/index.php
+++ studyplanner/index.php
@@ -4,6 +4,6 @@ if (isset($_SESSION['user_id'])) {
     header("Location: pages/dashboard.php");
 } else {
     header("Location: pages/login.php");
+    echo " hi my name is komal kummari singh ";
 }
 exit();
-?>
